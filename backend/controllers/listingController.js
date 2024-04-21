7552f275e160921d9b37641684aa1723d958aa21
richod1 <frimpsup@gmail.com>
2024-04-21 14:03:00 +0000

    listing controller added

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -71,6 +71,41 @@ const getListing=async(req,res,next)=>{
 
 const getListings=async(req,res,next)=>{
     try{
+        const limit=parseInt(req.query.limit) || 9;
+        const startIndex=paeseInt(req.query.startIndex) ||0;
+        let offer=req.query.offer;
+
+        if(offer ===undefined || offer == 'false'){
+            offer={$in:[false,true]};
+        }
+
+        let furnished=req.query.furnished;
+
+        if(furnished===undefined || furnished ==='false'){
+            furnished={$in:[false,true]};
+        }
+
+        let type=req.query.type;
+        if(type===undefined || type==='all'){
+            type={$in:['sale','rent']};
+        }
+
+        const serachTerm=req.query.serachTerm||'';
+        const sort=req.query.sort||'createdAt';
+        const order=req.query.order||'desc';
+
+
+        const listings=await Listing.find({
+            name:{$regex:serachTerm,$options:'1'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort({[sort]:order})
+        .limit(limit)
+        .skip(startIndex)
+
+        return res.status(200).json(listings);
 
     }catch(err){
         console.log('something went wrong at geting listing')
