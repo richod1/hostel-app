390f665e5ca18d6531470f48d18d3c1f0c8357fd
richod1 <frimpsup@gmail.com>
2024-04-21 12:29:10 +0000

    listing controller added

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -35,7 +35,43 @@ const deleteListing=async(req,res,next)=>{
     }
 }
 
+const updateListing=async(req,res,next)=>{
+    const listing=await Listing.findById(req.params.id)
+    if(!listing){
+        return next(errorHandler(404,'Listing not found'))
+    }
+
+    if(req.user.id!== req.param.userRef){
+        return next(errorHandler(401,'You can only update your own listing!'))
+    }
+    try{
+        const updateListing=await Listing.findByIdAndUpdate(
+            req.params.id,req.body,{new:true}
+        )
+
+        res.status(200).json(updateListing)
+
+    }catch(error){
+        console.log('something went wrong at update listing!',error)
+        next(error)
+    }
+}
+
+const getListing=async(req,res,next)=>{
+    try{
+        const listing=await Listing.findById(req.params.id)
+        if(!listing){
+            return next(errorHandler(404,'Listing not found!'))
+        }
+        res.status(200).json(listing)
+    }catch(error){
+        next(error)
+    }
+}
+
 module.exports={
     createListing,
     deleteListing,
+    updateListing,
+    getListing,
 }
