78191ca0b22ebc438cf831aa8bbcb36f3702d16e
richod1 <frimpsup@gmail.com>
2024-04-22 13:07:56 +0000

    cookies added

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,47 @@ const cors=require("cors")
 const BodyParser=require("body-parser")
 const port=process.env.PORT||3000
 require("dotenv").config()
+const path=require("node:path")
+const cookieParser=require("cookie-parser")
 const app=express()
 // database connection
 const mongoose=require("mongoose")
+const authRouter=require("./route/auth.route")
+const userRouter=require("./route/user.route")
+const listRouter=require("./route/listing.route")
+
 
 app.use(cors())
 app.use(BodyParser.json({extends:true}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 app.get('/api',(req,res)=>{
     res.status(200).json({message:"api is ready"})
 })
+
+
+app.use('/api/user',userRouter)
+app.use("/api/auth",authRouter)
+app.use('/api/listing',listRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
+app.use((err,req,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal Server Error';
+    return res.status(statusCode),json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
+
 
 
 app.listen(port,(err)=>{
